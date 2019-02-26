6a100e90a4e57719009a93fa157bc18c98b1dc9e
Cristina Grim <cristinaponce.designer@gmail.com>
2019-02-26 23:13:30 +0100

    Modify configuration of Circle CI

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,18 +1,12 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Form />
   </div>
 </template>
 
 <script>
-import Form from '@/components/Form.vue';
 
 export default {
   name: 'About',
-
-  components: {
-    Form,
-  },
 };
 </script>
